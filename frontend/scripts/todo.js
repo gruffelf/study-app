164f75e7e7ff211daa40edf04e20990cfdefaeb7
Aiden Torpy <aiden.torps@gmail.com>
2025-06-04 04:05:52 +1000

    Added the task submit button adds a div with textbox value

--- frontend/scripts/todo.js
+++ frontend/scripts/todo.js
@@ -1,6 +1,13 @@
 dim = document.getElementById("dim-overlay");
 taskEntry = document.getElementById("task-entry");
 
+studyList = document.getElementById("study-task-list");
+assessList = document.getElementById("assess-task-list");
+
+taskName = document.getElementById("task-name");
+
+taskHTML = `<div class='task'>- ${taskName.value}</div>`
+
 function openTask() {
   dim.style.display = "block";
   taskEntry.style.display = "flex";
@@ -9,4 +16,7 @@ function openTask() {
 function saveTask() {
   dim.style.display = "none";
   taskEntry.style.display = "none";
+
+  studyList.innerHTML += taskHTML;
+
 }
